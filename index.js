c0ab6b2c121c62bb33ff6af2fed0236b041ead05
SPARSH1\PCQ154 <kinjal.tatvasoft@gmail.com>
2018-10-16 10:32:17 +0530

    change in path of the imported file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import store from './src/redux/store';
 import { AppRegistry } from 'react-native';
 import React, {Component} from 'react';
 
-import AppViewContainer from './src/modules/AppViewContainer';
+import AppViewContainer from './src/containers/AppViewContainer';
 
 class News extends Component {
     render() {
